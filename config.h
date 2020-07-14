dcedfe80a901f361b1568b52006dd4be5896c72f
Peter Gomez <fadedlamp42@gmail.com>
2020-07-14 10:50:20 -0400

    added horizontal layout to mod+shift+h

--- config.h
+++ config.h
@@ -39,6 +39,7 @@ static const Rule rules[] = {
 	{ NULL,       NULL,       "Base Converter",             0,          1,           -1},
 };
 
+#include "horizgrid.c"
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
@@ -49,6 +50,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "###",      horizgrid },
 };
 
 /* key definitions */
@@ -96,6 +98,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,          setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,          setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,          setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_h,          setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,      setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,      togglefloating, {0} },
 	{ MODKEY,                       XK_0,          view,           {.ui = ~0 } },
