4e1da0cd8381381043cdb4715d307dc50098c1db
Peter Gomez <fadedlamp42@gmail.com>
2020-10-20 16:55:01 -0400

    fix default value for setgaps

--- dwm.c
+++ dwm.c
@@ -1846,7 +1846,7 @@ void
 setgaps(const Arg *arg)
 {
 	if (arg->i == 0)
-		selmon->gappx = 17; //default value
+		selmon->gappx = 50; //default value
 	else if (selmon->gappx + arg->i < 0)
 		selmon->gappx = 0; //minimum 0
 	else
